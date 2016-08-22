c1f3ff331132f595ce6cd4f16950c6eb50b42c06
Claudiney Nascimento <claudineydelima@gmail.com>
2016-08-22 20:59:52 -0300

    Mixins e polimorfismo

--- ebook.rb
+++ ebook.rb
@@ -1,5 +1,7 @@
-class EBook
-attr_reader :titulo, :preco, :ano_lancamento, :editora
+require_relative "produto"
+
+class EBook 
+include Produto
 
   def initialize(titulo, preco, ano_lancamento, editora)
     @titulo = titulo
@@ -8,27 +10,8 @@ attr_reader :titulo, :preco, :ano_lancamento, :editora
     @editora = editora
   end
 
-  def to_csv
-  	"#{@titulo}, #{@ano_lancamento}, #{@preco}"  	
-  end
-
-
 	def matches?(query)
 		["ebook", "digital"].include?(query)
 	end
 
-private
-	def calcula_preco(base)
-		if @ano_lancamento < 2006
-			if @possui_reimpressao
-				base*0.9
-			else
-				base*0.95
-			end
-		elsif @ano_lancamento<=2010
-			base * 0.96
-		else
-			base
-		end
-	end
 end
--- revista.rb
+++ revista.rb
@@ -1,6 +1,8 @@
-class Revista
-attr_reader :titulo, :preco, :ano_lancamento, :editora
+require_relative "produto"
 
+class Revista
+
+	include Produto
   def initialize(titulo, preco, ano_lancamento, possui_reimpressao, numero, editora)
     @titulo = titulo
     @ano_lancamento = ano_lancamento
@@ -10,9 +12,6 @@ attr_reader :titulo, :preco, :ano_lancamento, :editora
     @editora = editora
   end
 
-  def to_csv
-  	"#{@titulo}, #{@ano_lancamento}, #{@preco}"  	
-  end
 
   def possui_reimpressao?
     @possui_reimpressao
@@ -22,18 +21,4 @@ attr_reader :titulo, :preco, :ano_lancamento, :editora
 		["revista", "impresso"].include?(query)
 	end
 
-private
-	def calcula_preco(base)
-		if @ano_lancamento < 2006
-			if @possui_reimpressao
-				base*0.9
-			else
-				base*0.95
-			end
-		elsif @ano_lancamento<=2010
-			base * 0.96
-		else
-			base
-		end
-	end
 end
