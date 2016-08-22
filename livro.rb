41a5d7156ef2859f6868d71e5ebddf4f1f2bc767
Claudiney Nascimento <claudineydelima@gmail.com>
2016-08-22 12:12:39 -0300

    Sobre modelos anemicos

--- livro.rb
+++ livro.rb
@@ -1,25 +1,33 @@
 class Livro
-	attr_reader :titulo, :preco,:ano_lancamento
-	
-	def initialize(titulo, preco, ano_lancamento, possui_reimpressao)
-		@titulo = titulo
-		@preco = calcula_preco preco
-		@ano_lancamento = ano_lancamento
-		@possui_reimpressao = possui_reimpressao
-	end
+attr_reader :titulo, :preco, :ano_lancamento
 
-	def possui_reimpressao?
-		@possui_reimpressao		
-	end
+  def initialize(titulo, preco, ano_lancamento, possui_reimpressao)
+    @titulo = titulo
+    @ano_lancamento = ano_lancamento
+    @preco = calcula_preco(preco)
+    @possui_reimpressao = possui_reimpressao
+  end
+
+  def to_csv
+  	"#{@titulo}, #{@ano_lancamento}, #{@preco}"  	
+  end
+
+  def possui_reimpressao?
+    @possui_reimpressao
+  end
 
 private
 	def calcula_preco(base)
-		if @ano_lancamento<2000
-			base *0.7
+		if @ano_lancamento < 2006
+			if @possui_reimpressao
+				base*0.9
+			else
+				base*0.95
+			end
+		elsif @ano_lancamento<=2010
+			base * 0.96
 		else
 			base
 		end
 	end
-
 end
-
--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require_relative "livro"
+require_relative "estoque"
 
 
 def imprime_nota_fiscal(livros)
@@ -15,13 +16,26 @@ def livro_para_newsletter(livro)
 	end
 end
 
-agile = Livro.new("Agile Web Development with Rails", 70, 2011)
-livro_ruby = Livro.new("Programming Ruby 1.9", 60, 2010)
+agile = Livro.new("Agile Web Development with Rails", 70, 2011, true)
+livro_ruby = Livro.new("Programming Ruby 1.9", 60, 2010, true)
 algoritmos = Livro.new("Algoritmos", 100, 1998, true)
-livro_para_newsletter algoritmos
+#livro_para_newsletter algoritmos
 
 livros = [agile, livro_ruby, algoritmos]
 
 #imprime_nota_fiscal livros
 
+estoque = Estoque.new
 
+estoque.livros<<agile <<algoritmos<<livro_ruby
+estoque.livros << Livro.new("The Pragmatic Programmer", 100, 1999, true)
+estoque.livros << Livro.new("Programming Ruby", 100, 2004, true)
+#estoque.livros << nil
+
+
+estoque.exporta_csv
+puts estoque.total
+baratos = estoque.mais_barato_que 80
+baratos.each do |livro|
+	puts livro.titulo
+end
